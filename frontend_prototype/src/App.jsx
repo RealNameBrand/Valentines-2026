03d4aa24c9f39bfb01afdf1d165ac3f4ce16401c
Brody <brody.hillmail@gmail.com>
2026-02-07 21:27:08 -0700

    fixed photo sizes

--- frontend_prototype/src/App.jsx
+++ frontend_prototype/src/App.jsx
@@ -9,27 +9,62 @@ function App() {
 
     return (
         <div className="app-container">
-            {/* Main Content */}
-            <main>
-                {!started ? (
-                    <Home onStart={() => setStarted(true)} />
-                ) : (
-                    <SwipeDeck />
-                )}
-            </main>
+            {!started ? (
+                <Home onStart={() => setStarted(true)} />
+            ) : (
+                <SwipeDeck />
+            )}
 
-            {/* Social Links Footer */}
             <footer className="social-footer">
                 <a href="https://www.instagram.com/_brody.h/" target="_blank" rel="noreferrer">
                     <img
                         src={instaIcon}
-                         alt="Instagram"
+                        alt="Instagram"
                         style={{ width: '30px', height: '30px' }}
                     />
                 </a>
             </footer>
         </div>
+
+
     )
 }
 
-export default App
+// export default App
+
+
+// import React, { useState } from 'react'
+// import Home from './components/Home'
+// import SwipeDeck from './components/SwipeDeck'
+// import instaIcon from './assets/InstagramIcon.png' // Import your image here
+// import './styles/global.css'
+
+// function App() {
+//     const [started, setStarted] = useState(false)
+
+//     return (
+//         <div className="app-container">
+//             {/* Main Content */}
+//             <main>
+//                 {!started ? (
+//                     <Home onStart={() => setStarted(true)} />
+//                 ) : (
+//                     <SwipeDeck />
+//                 )}
+//             </main>
+
+//             {/* Social Links Footer */}
+//             <footer className="social-footer">
+//                 <a href="https://www.instagram.com/_brody.h/" target="_blank" rel="noreferrer">
+//                     <img
+//                         src={instaIcon}
+//                         alt="Instagram"
+//                         style={{ width: '30px', height: '30px' }}
+//                     />
+//                 </a>
+//             </footer>
+//         </div>
+//     )
+// }
+
+// export default App
