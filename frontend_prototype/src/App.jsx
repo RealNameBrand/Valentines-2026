de476af3a6d52e2e13a71e992c6d20605f1abfa1
Brody <brody.hillmail@gmail.com>
2026-02-07 21:09:52 -0700

    changed out photos and added socials

--- frontend_prototype/src/App.jsx
+++ frontend_prototype/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Home from './components/Home'
 import SwipeDeck from './components/SwipeDeck'
+import instaIcon from './assets/InstagramIcon.png' // Import your image here
 import './styles/global.css'
 
 function App() {
@@ -8,11 +9,25 @@ function App() {
 
     return (
         <div className="app-container">
-            {!started ? (
-                <Home onStart={() => setStarted(true)} />
-            ) : (
-                <SwipeDeck />
-            )}
+            {/* Main Content */}
+            <main>
+                {!started ? (
+                    <Home onStart={() => setStarted(true)} />
+                ) : (
+                    <SwipeDeck />
+                )}
+            </main>
+
+            {/* Social Links Footer */}
+            <footer className="social-footer">
+                <a href="https://www.instagram.com/_brody.h/" target="_blank" rel="noreferrer">
+                    <img
+                        src={instaIcon}
+                         alt="Instagram"
+                        style={{ width: '30px', height: '30px' }}
+                    />
+                </a>
+            </footer>
         </div>
     )
 }
