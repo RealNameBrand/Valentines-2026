b255b7277d3a9b17fbaab9eb3ed8cdd6d3da540f
Brody <brody.hillmail@gmail.com>
2026-02-07 21:40:59 -0700

    it wont deploy

--- frontend_prototype/src/App.jsx
+++ frontend_prototype/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import Home from './components/Home'
 import SwipeDeck from './components/SwipeDeck'
-// import instaIcon from './assets/' // Import your image here
 import './styles/global.css'
 
 function App() {
@@ -15,7 +14,8 @@ function App() {
                 <SwipeDeck />
             )}
 
-            {/* <footer className="social-footer">
+            
+            <footer className="social-footer">
                 <a href="https://www.instagram.com/_brody.h/" target="_blank" rel="noreferrer">
                     <img
                         src='./assets/instagram-logo.png'
@@ -23,48 +23,9 @@ function App() {
                         style={{ width: '30px', height: '30px' }}
                     />
                 </a>
-            </footer> */}
+            </footer>
         </div>
-
-
     )
 }
 
-// export default App
-
-
-// import React, { useState } from 'react'
-// import Home from './components/Home'
-// import SwipeDeck from './components/SwipeDeck'
-// import instaIcon from './assets/InstagramIcon.png' // Import your image here
-// import './styles/global.css'
-
-// function App() {
-//     const [started, setStarted] = useState(false)
-
-//     return (
-//         <div className="app-container">
-//             {/* Main Content */}
-//             <main>
-//                 {!started ? (
-//                     <Home onStart={() => setStarted(true)} />
-//                 ) : (
-//                     <SwipeDeck />
-//                 )}
-//             </main>
-
-//             {/* Social Links Footer */}
-//             <footer className="social-footer">
-//                 <a href="https://www.instagram.com/_brody.h/" target="_blank" rel="noreferrer">
-//                     <img
-//                         src={instaIcon}
-//                         alt="Instagram"
-//                         style={{ width: '30px', height: '30px' }}
-//                     />
-//                 </a>
-//             </footer>
-//         </div>
-//     )
-// }
-
-// export default App
+export default App
